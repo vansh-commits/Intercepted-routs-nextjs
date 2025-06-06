b9112b3133de8859abd931deb71a16fd4e8994b2
vansh-commits <vansh.malhotra439@hotmail.com>
2025-06-06 11:02:54 +0530

    added data

---
+++ src/app/db/data.ts
@@ -0,0 +1,22 @@
+export const games = [
+  {
+    id: 1,
+    name: "Cyberpunk 2077",
+    img: "https://variety.com/wp-content/uploads/2023/10/cyberpunk.jpeg?w=1000&h=667&crop=1",
+  },
+  {
+    id: 2,
+    name: "The Witcher 3",
+    img: "https://sm.ign.com/ign_za/preview/w/we-played-/we-played-the-witcher-3-wild-hunt-for-6-hours-ign_3v48.jpg",
+  },
+  {
+    id: 3,
+    name: "Red Dead Redemption 2",
+    img: "https://platform.polygon.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/10137177/49c7d29293f89dfef95969fde62f5c27e7cc780c.jpg?quality=90&strip=all&crop=7.8125,0,84.375,100",
+  },
+  {
+    id: 4,
+    name: "Elden Ring",
+    img: "https://static.bandainamcoent.eu/high/elden-ring/elden-ring/00-page-setup/elden-ring-new-header-mobile.jpg",
+  },
+];
