bd31b5238dec46ad076845197a92f93261cbcfab
vansh-commits <vansh.malhotra439@hotmail.com>
2025-06-06 11:14:04 +0530

    created components

---
+++ src/app/games/page.tsx
@@ -0,0 +1,19 @@
+import Sidebar from '@/components/Sidebar'
+import React from 'react'
+
+const Games = () => {
+  return (
+    <div>
+        <Sidebar />
+        <div className="flex-1 bg-gray-100 p-5">
+            <h1 className="text-2xl font-bold mb-4">Featured Games</h1>
+            <div className="flex gap-4">
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Games
